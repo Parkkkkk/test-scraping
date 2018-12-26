6eb15639d41997206c2aa991a3fbb96c84351e6f
hyeock gu <mb7878@naver.com>
2018-12-26 20:27:39 +0900

    4study

---
+++ router/scraping.js
@@ -0,0 +1,47 @@
+const express = require('express');
+const cheerio = require('cheerio');
+const request = require('request');
+
+
+
+const url = "http://www.dumaro.co.kr"
+const router = express.Router();
+var data = [];
+
+
+router.get('/scraping' , (req, res, next) => {
+    request(url, (error,response,html) => {
+        if(!error && response.statusCode == 200) {
+         const $ = cheerio.load(html);
+
+         $('.ckc').each((i, el) => {
+            
+             var id = parseInt(i + 1); 
+
+             //해당 item url crawling
+             var item = $(el)
+             .find('.box a')
+             .attr('href');
+
+             //image crawling
+             var image = $(el)
+             .find('.sum img')
+             .attr('src');
+
+             data[i] = 
+                 {
+                     id : id,
+                     item : `${url}${item}`,
+                     image : image
+                 };
+                })
+            console.log(data);
+            res.json(data);
+        } else {
+            console.log('fail');
+        }
+    })
+})
+
+module.exports = router;
+
--- server.js
+++ server.js
@@ -1,48 +1,11 @@
 const express = require('express');
-const request = require('request');
-const cheerio = require('cheerio');
-
-
+const api = require('./router/scraping');
 
 const app = express();
 const PORT = process.env.PORT || 3030;
-const url = "";
 
-const data = [];
 
-app.get('/api/scraping' , (req, res, next) => {
-    request(url, (error,response,html) => {
-        if(!error && response.statusCode == 200) {
-         const $ = cheerio.load(html);
-
-         $('.ckc').each((i, el) => {
-            
-             var id = parseInt(i + 1); 
-
-             //해당 item url crawling
-             var item = $(el)
-             .find('.box a')
-             .attr('href');
-
-             //image crawling
-             var image = $(el)
-             .find('.sum img')
-             .attr('src');
-
-             data[i] = 
-                 {
-                     id : id,
-                     item : `${url}${item}`,
-                     image : image
-                 };
-                })
-            console.log(data);
-            res.json(data);
-        } else {
-            console.log('fail');
-        }
-    })
-})
+app.use('/api', api);
 
 
 app.listen(PORT, () => {
